f3166a729af493d3d855aa37c3a4ae09d38fbb7b
iWECon <i.1214@yahoo.com>
2016-03-21 10:52:18 +0800

    fix1

---
+++ NetWorking/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  NetWorking
+//
+//  Created by iWe on 2016/3/18.
+//  Copyright © 2016年 iWe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
